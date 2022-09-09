bb62f27dd29b396865d027f4db1eed32ec822924
Roman Radakhevych <akrir@ukr.net>
2022-09-09 22:43:24 +0300

    app routes

---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,15 @@
+import { Link, Outlet } from "react-router-dom"
+
+export const SharedLayout = () => {
+    return (
+        <>
+            <header>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/movies">Movies</Link>
+                </nav>
+            </header>
+            <Outlet />
+        </>
+    )
+}
